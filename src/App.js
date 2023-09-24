366744206b62dc354d1513891441f92b3b2a33be
asif <asifmansuri736@gmail.com>
2023-09-24 14:39:40 +0530

    Updated files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Main from '../src/Containers/Main';
+import { createTheme } from '@material-ui/core/styles';
+const theme = createTheme({
+  palette: {
+    primary: { main: '#64dd17' },
+    secondary: { main: '#64dd17' }
+  },
+  overrides: {
+    MuiPickersToolbar: {
+      toolbar: {
+        backgroundColor: '#64dd17',
+      },
+    },
+    MuiPickersToolbarText: {
+      toolbarTxt: {
+        color: '#fff'
+      },
+      toolbarBtnSelected: {
+        color: '#fff'
+      }
+    },
+    MuiPickersDay: {
+      daySelected: {
+        color: '#fff'
+      }
+    },
+    MuiPickersClockNumber: {
+      clockNumberSelected: {
+        color: '#fff'
+      }
+    }
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <MuiThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          {/* <Route path="/profile" element={<Profile />} /> */}
+          {/* Add more routes here */}
+        </Routes>
+      </MuiThemeProvider>
+    </Router>
   );
 }
 
